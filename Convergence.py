Fixed weighting function.

Convergence.py
@@ -438,16 +438,16 @@ def distance_ratio(z_source):
     _, chis, zs, _ = create_chi_bins(0, z_source, 1002)
     z_source = zs[-1]
     # z_arr = np.linspace(0, z_source, 1001)
-    D_S = b_comoving(0, z_source)[-1] / (1+z_source)
-    chi_S = b_comoving(0, z_source)[-1]
-    D_L = np.array([(b_comoving(0, i)[-1] / (1+i)) for i in zs])
-    chi_L = np.array([(b_comoving(0, i)[-1]) for i in zs])
-    D_LS = np.array([((b_comoving(0, z_source)[-1] - b_comoving(0, i)[-1]) / (1+z_source)) for i in zs])
+    D_S = b_comoving(0, z_source, 1, 0, 1001)[-1] / (1+z_source)
+    chi_S = b_comoving(0, z_source, 1, 0, 1001)[-1]
+    D_L = np.array([(b_comoving(0, i, 1, 0, 1001)[-1] / (1+i)) for i in zs])
+    chi_L = np.array([(b_comoving(0, i, 1, 0, 1001)[-1]) for i in zs])
+    D_LS = np.array([((b_comoving(0, z_source, 1, 0, 1001)[-1] - b_comoving(0, i, 1, 0, 1001)[-1]) / (1+z_source)) for i in zs])
     D_ratio = D_L * D_LS / D_S
     chi_ratio = chi_L * (np.linspace(chi_S, chi_S, 1001) - chi_L) / np.linspace(chi_S, chi_S, 1001) * (1 + zs)
     D_A = comoving(zs) / (1 + zs)
     z_peak = np.array(zs)[np.argmin(np.abs(D_ratio - max(D_ratio)))]
-    z_peak2 = np.array(zs)[np.argmin(np.abs(chi_ratio - max(chi_ratio)))]
+    z_peak_chi = np.array(zs)[np.argmin(np.abs(chi_ratio - max(chi_ratio)))]
     plt.plot(zs, np.linspace(chi_S, chi_S, 1001), color=[0.75, 0.75, 0.75], linestyle='--', label='$\chi_S$')
     plt.plot(zs, chi_L, color=colours[0], label='$\chi_L$')
     plt.plot(zs, (np.linspace(chi_S, chi_S, 1001) - chi_L), color=colours[1], label='$\chi_{LS}$')
@@ -456,22 +456,22 @@ def distance_ratio(z_source):
     plt.legend(frameon=0)
     # plt.plot(z_peak, max(D_ratio), marker='x', color=colours[2])
     # plt.text(z_peak, D_S / 4, f'$z$ = {round(z_peak, 2)}', fontsize=16, ha='center', color=colours[2])
-    plt.plot(z_peak2, max(chi_ratio), marker='x', color=colours[2])
-    plt.text(z_peak2, chi_S / 3.5, f'$z$ = {round(z_peak2, 2)}', fontsize=16, ha='center', color=colours[2])
+    plt.plot(z_peak_chi, max(chi_ratio), marker='x', color=colours[2])
+    plt.text(z_peak_chi, chi_S / 3.5, f'$z$ = {round(z_peak_chi, 4)}', fontsize=16, ha='center', color=colours[2])
     plt.xlabel('$z$')
-    plt.ylabel('$\chi$ (Gpc)')
+    plt.ylabel('$D_A$ (Gpc)')
     plt.show()
 
-    chi_peak = np.array(chis)[np.argmin(np.abs(D_ratio - max(D_ratio)))]
+    z_peak_D = np.array(zs)[np.argmin(np.abs(D_ratio - max(D_ratio)))]
     chi_peak2 = np.array(chis)[np.argmin(np.abs(chi_ratio - max(chi_ratio)))]
-    plt.plot(chis, np.linspace(D_S, D_S, 1001), color=[0.75, 0.75, 0.75], linestyle='--', label='$D_S$')
-    plt.plot(chis, D_L, color=colours[0], label='$D_L$')
-    plt.plot(chis, D_LS, color=colours[1], label='$D_{LS}$')
-    plt.plot(chis, D_ratio, color=colours[2], label='$D_LD_{LS}/D_S$')
+    plt.plot(zs, np.linspace(D_S, D_S, 1001), color=[0.75, 0.75, 0.75], linestyle='--', label='$D_S$')
+    plt.plot(zs, D_L, color=colours[0], label='$D_L$')
+    plt.plot(zs, D_LS, color=colours[1], label='$D_{LS}$')
+    plt.plot(zs, D_ratio, color=colours[2], label='$D_LD_{LS}/D_S$')
     # plt.plot(chis, chi_ratio, color=colours[4], label='$\chi_L\chi_{LS}/\chi_Sa_L$')
     plt.legend(frameon=0)
-    plt.plot(chi_peak, max(D_ratio), marker='x', color=colours[2])
-    plt.text(chi_peak, D_S / 5, f'$\chi$ = {round(chi_peak, 2)} Gpc', fontsize=16, ha='center', color=colours[2])
+    plt.plot(z_peak_D, max(D_ratio), marker='x', color=colours[2])
+    plt.text(z_peak_D, D_S / 5, f'$\chi$ = {round(z_peak_D, 4)} Gpc', fontsize=16, ha='center', color=colours[2])
     # plt.plot(chi_peak2, max(chi_ratio), marker='x', color=colours[4])
     # plt.text(chi_peak2, chi_S / 2.5, f'$\chi$ = {round(chi_peak2, 2)} Gpc', fontsize=16, ha='center', color=colours[4])
     plt.xlabel('$z$')

cones.py
@@ -145,10 +145,10 @@ def sort_SN_gals(cut_data, redo=False):
         for num, SRA, SDE, SZ, SM, SE, C in zip(np.linspace(0, len(RA2) - 1, len(RA2)), RA2, DEC2, z2, mu, mu_err, CID):
             lenses[f'SN{int(num)+1}'] = {'RAs': [], 'DECs': [], 'Zs': [], 'SNZ': SZ, 'SNMU': SM, 'SNMU_ERR': SE,
                                          'SNRA': SRA, 'SNDEC': SDE, 'WEIGHT': 1, 'CID': C}
-            if SDE > 1.01:
-                h = SDE - 1.01
-            elif SDE < -1.01:
-                h = -1.01 - SDE
+            if SDE > 1.1:
+                h = SDE - 1.1
+            elif SDE < -1.1:
+                h = -1.1 - SDE
             else:
                 h = 0
             theta = 2 * np.arccos(1 - h / 0.2)
@@ -218,7 +218,6 @@ def plot_cones(cut_data, lenses, patches):
     plt.axis('equal')
     plt.xlim([24.5, 27.5])
     plt.ylim([-1, 1])
-    plt.tick_params(axis='both', which='both', top=False, bottom=False, left=False, right=False)
     plt.show()
 
     # labels = ['Galaxies', 'Supernovae']
@@ -507,13 +506,14 @@ def find_convergence(lenses, SNz, cut2, limits, plot=False):
                                             exp[:len(SN)], chiSNs[num])
         num += 1
 
-    SN645 = counts["SN646"]
-    plt.plot(zs[:len(SN645)], SN645, marker='o')
-    plt.plot(zs[:len(SN645)], exp[:len(SN645)], marker='o')
-    plt.plot(zs[:len(SN645)], d_arr["SN646"], marker='o')
-    plt.text(0.0, 6, f'$\kappa$ = {round(convergence[645], 4)}')
-    plt.text(0, 4, f"($\\alpha$, $\delta$), ({round(lenses['SN646']['SNRA'],2)}, {round(lenses['SN646']['SNDEC'],2)})")
-    plt.text(0, 2, f"CID {lenses['SN646']['CID']}")
+    SN = counts["SN669"]
+    plt.plot(zs[:len(SN)], SN, marker='o')
+    plt.plot(zs[:len(SN)], exp[:len(SN)], marker='o')
+    plt.plot(zs[:len(SN)], d_arr["SN669"], marker='o')
+    plt.text(0.0, 6, f'$\kappa$ = {round(convergence[668], 4)}')
+    plt.text(0, 4, f"($\\alpha$, $\delta$), ({round(lenses['SN669']['SNRA'],2)}, {round(lenses['SN669']['SNDEC'],2)})")
+    plt.text(0, 2, f"CID {lenses['SN669']['CID']}")
+    # plt.ylim([-2, 8])
 
     plt.plot([0, 0.3], [0, 0], linestyle='--', color=[0.5, 0.5, 0.5])
     plt.show()
@@ -678,7 +678,7 @@ def find_correlation(conv, mu_diff):
 
 if __name__ == "__main__":
     data, S_data, circles = get_data(new_data=False)
-    lensing_gals = sort_SN_gals(data, redo=True)
+    lensing_gals = sort_SN_gals(data, redo=False)
     # plot_cones(data, lensing_gals, circles)
     cone_array = make_test_cones(data, redo=False)
     bin_limits, exp, chi_widths, chis, zs = find_expected_counts(cone_array, 51, redo=False)
@@ -705,9 +705,9 @@ if __name__ == "__main__":
     convergence_cut = find_convergence(lensing_gals, SNzs, cuts2, bin_limits, plot=True)
     plt.plot(S_data['kappa'], S_data['kappa'], color=colours[1])
     plt.plot(S_data['kappa'], convergence_cut, color=colours[0], marker='o', markersize=3, linestyle='')
-    # for i in range(len(convergence_cut)):
-    #     if convergence_cut[i] > 0.02:
-    #         print(f"Outlier is SN {i}/{len(convergence_cut)}")
+    for i in range(len(convergence_cut)):
+        if convergence_cut[i] < 0.0031 and S_data['kappa'][i] > 0.0057:
+            print(f"Outlier is SN {i}/{len(convergence_cut)}")
     plt.xlabel('$\kappa$ Smith')
     plt.ylabel('My $\kappa$')
     plt.show()